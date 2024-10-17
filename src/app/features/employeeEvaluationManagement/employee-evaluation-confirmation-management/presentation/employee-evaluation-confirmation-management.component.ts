ec7938421f1e0295bc92421e5a0949616fd1cc44
Aboubaker Alaghl <abobkerelaghel>
2024-10-17 15:30:22 +0200

    add EmployeeEvaluationManagementConfirmation page

--- src/app/features/employeeEvaluationManagement/employee-evaluation-confirmation-management/presentation/employee-evaluation-confirmation-management.component.ts
+++ src/app/features/employeeEvaluationManagement/employee-evaluation-confirmation-management/presentation/employee-evaluation-confirmation-management.component.ts
@@ -9,67 +9,7 @@ declare var $: any;
   styleUrls: ['./employee-evaluation-confirmation-management.component.scss']
 })
 
-
 // export default class SecondmentToOtherPostionComponent {}
 export default class EmployeeEvaluationConfirmationComponentComponent implements OnInit {
-  edit: boolean = false;
-
-  registerForm = this.fb.group({
-    id: [''],
-    name: ['', Validators.required]
-  });
-
-  constructor(private fb: FormBuilder,
-              // protected banksFacade: BanksFacade,
-              private sharedFacade: SharedFacade,
-              private cdr: ChangeDetectorRef
-            
-            ) {
-    this.onSubmit();
-
-  }
-
-  ngOnInit() {
-    this.edit = false;
-  }
-
-  onSubmit(): void {
-    // this.registerForm.controls.id.setValue('');
-    // this.banksFacade.GetBanks();
-  }
-
-  onDelete(Id: string): void {
-    // this.edit = false;
-    // this.banksFacade.deleteBank(Id);
-    // this.registerForm.reset();
-  }
-  onReset(): void {
-    // this.edit = false;
-    // this.registerForm.reset();
-    // this.registerForm.setErrors(null);
-  }
-
-  onAdd(): void {
-    // if (this.registerForm.valid) {
-    //   if (this.edit) {
-    //     this.banksFacade.UpdateBank(this.registerForm?.value);
-    //     this.onReset();
-    //   } else {
-    //     this.banksFacade.AddBank(this.registerForm?.value);
-    //     this.onReset();
-
-    //   }
-    // }else {
-    //   this.showNotification('عفواً، الرجاء ادخال اسم المصرف','');
-    // }
-  }
-  showNotification(title, text){
-    // this.sharedFacade.showMessage(MessageType.warning, title, ['']);
-  }
-
-  onEdit(bank: any): void {
-    // this.registerForm.patchValue(bank);
-    // this.edit = true;
-  }
-
+  ngOnInit(): void {}
 }
