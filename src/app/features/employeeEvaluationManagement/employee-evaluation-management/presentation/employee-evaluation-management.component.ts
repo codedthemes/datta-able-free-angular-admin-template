a5ef21fe036d5fc7992e250adee115e16c70a13d
Aboubaker Alaghl <abobkerelaghel>
2024-10-21 10:03:37 +0200

    add missing days

--- src/app/features/employeeEvaluationManagement/employee-evaluation-management/presentation/employee-evaluation-management.component.ts
+++ src/app/features/employeeEvaluationManagement/employee-evaluation-management/presentation/employee-evaluation-management.component.ts
@@ -13,31 +13,40 @@ export default class EmployeeEvaluationManagementComponent implements OnInit, On
   currentTab: EmployeeEvaluationManagementComponentTabs = 'General';
 
   metric1 = [
-    { name: "معرفة العمل و الإلمام بالجوانب الفنية المتعلقة بها", score: 80, randomValue1: 75, randomValue2: 62 },
-    { name: "المقدرة على وضع الاولويات في العمل", score: 70, randomValue1: 59, randomValue2: 42 },
-    { name: "الدقة والسرعة في إنجاز الاعمال بانقل نسبة ممكنة من الأخطاء", score: 70, randomValue1: 68, randomValue2: 52 },
-    { name: "المقدرة على أداء العمل بدون رقابة أو متابعة", score: 80, randomValue1: 73, randomValue2: 60 },
-    { name: "درجة الاعتماد عليه", score: 70, randomValue1: 64, randomValue2: 53 },
-    { name: "تقييم الأفكار والمقترحات", score: 60, randomValue1: 48, randomValue2: 29 },
-    { name: "المحافظة على معدات و أدوات العمل", score: 50, randomValue1: 44, randomValue2: 30 },
-    { name: "التعامل مع صعوبات العمل", score: 50, randomValue1: 38, randomValue2: 25 },
-    { name: "إتباع قواعد الأمن والسلامة", score: 50, randomValue1: 41, randomValue2: 32 },
-    { name: "نقل آراء الآخرين ومناقشتها", score: 60, randomValue1: 50, randomValue2: 45 },
-    { name: "المواظبة والمحافظة على مواعيد العمل", score: 50, randomValue1: 43, randomValue2: 37 },
-    { name: "القابلية للتدريب والالتزام بحضور الدورات التدريبية", score: 40, randomValue1: 31, randomValue2: 28 }
+    { name: 'معرفة العمل و الإلمام بالجوانب الفنية المتعلقة بها', score: 80, randomValue1: 75, randomValue2: 62 },
+    { name: 'المقدرة على وضع الاولويات في العمل', score: 70, randomValue1: 59, randomValue2: 42 },
+    { name: 'الدقة والسرعة في إنجاز الاعمال بانقل نسبة ممكنة من الأخطاء', score: 70, randomValue1: 68, randomValue2: 52 },
+    { name: 'المقدرة على أداء العمل بدون رقابة أو متابعة', score: 80, randomValue1: 73, randomValue2: 60 },
+    { name: 'درجة الاعتماد عليه', score: 70, randomValue1: 64, randomValue2: 53 },
+    { name: 'تقييم الأفكار والمقترحات', score: 60, randomValue1: 48, randomValue2: 29 },
+    { name: 'المحافظة على معدات و أدوات العمل', score: 50, randomValue1: 44, randomValue2: 30 },
+    { name: 'التعامل مع صعوبات العمل', score: 50, randomValue1: 38, randomValue2: 25 },
+    { name: 'إتباع قواعد الأمن والسلامة', score: 50, randomValue1: 41, randomValue2: 32 },
+    { name: 'نقل آراء الآخرين ومناقشتها', score: 60, randomValue1: 50, randomValue2: 45 },
+    { name: 'المواظبة والمحافظة على مواعيد العمل', score: 50, randomValue1: 43, randomValue2: 37 },
+    { name: 'القابلية للتدريب والالتزام بحضور الدورات التدريبية', score: 40, randomValue1: 31, randomValue2: 28 }
   ];
 
   metric2 = [
-    { name: "تقبل التوجيهات والاستعداد لتنفيذها", score: 50, randomValue1: 34, randomValue2: 34 },
-    { name: "المحافظة على أسرار العمل والمستندات السرية", score: 35, randomValue1: 35, randomValue2: 40 },
-    { name: "القدرة على العمل تحت جملة من الضغوط", score: 40, randomValue1: 29, randomValue2: 29 },
-    { name: "تقبل التجديد في أساليب العمل", score: 40, randomValue1: 31, randomValue2: 31 },
-    { name: "السلوك العام والعناية بالمظهر بمايتناسب بطبيعة الوظيفة", score: 20, randomValue1: 17, randomValue2: 17 }
+    { name: 'تقبل التوجيهات والاستعداد لتنفيذها', score: 50, randomValue1: 34, randomValue2: 34 },
+    { name: 'المحافظة على أسرار العمل والمستندات السرية', score: 35, randomValue1: 35, randomValue2: 40 },
+    { name: 'القدرة على العمل تحت جملة من الضغوط', score: 40, randomValue1: 29, randomValue2: 29 },
+    { name: 'تقبل التجديد في أساليب العمل', score: 40, randomValue1: 31, randomValue2: 31 },
+    { name: 'السلوك العام والعناية بالمظهر بمايتناسب بطبيعة الوظيفة', score: 20, randomValue1: 17, randomValue2: 17 }
   ];
 
   metric3 = [
-    { name: "إنجاز أعمال مميزة على مستوى الشركة (تذكر في تقرير مرفق)", score: 60, randomValue1: 0, randomValue2: 0 },
-    { name: "حل بعض المشاكل المعقدة بالإدارة (تذكر في تقرير مرفق)", score: 50, randomValue1: 0, randomValue2: 0 }
+    { name: 'إنجاز أعمال مميزة على مستوى الشركة (تذكر في تقرير مرفق)', score: 60, randomValue1: 0, randomValue2: 0 },
+    { name: 'حل بعض المشاكل المعقدة بالإدارة (تذكر في تقرير مرفق)', score: 50, randomValue1: 0, randomValue2: 0 }
+  ];
+
+  metric4 = [
+    { name: 'الغياب الغير مشروع', symbol: 'X', count: 0 },
+    { name: 'الإجازات الغير مدفوعة الأجر بدون مرتب خلال السنة', symbol: 'Z', count: 0 },
+    { name: 'الغياب المشروع مدفوع الأجر خلال السنة', symbol: 'P', count: 5 },
+    { name: 'الإجازات المرضية خلال السنة', symbol: 'S', count: 0 },
+    { name: 'اللذب أو الأعارة خلال السنة', symbol: 'L', count: 0 },
+    { name: 'التدريب خلال السنة', symbol: 'E', count: 0 }
   ];
 
   private selectedEmployeeSub: Subscription;
