14771516b0106c75b04873a7cf40411f2447dc5e
Dikshit Moradiya <106728438+ct-dixit@users.noreply.github.com>
2025-05-22 15:16:14 +0530

    angular 20 and package upgrade and footer add (#33)

---
+++ src/app/theme/layout/admin/footer/footer.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.html',
+  styleUrl: './footer.scss'
+})
+export class Footer {}
